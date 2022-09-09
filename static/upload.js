183011d9fcc053c8c7b85712809922b120e5b78d
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-09-08 22:11:08 -0400

    Fix render scaling

--- static/upload.js
+++ static/upload.js
@@ -1,6 +1,11 @@
 async function render() {
   const renderContent = document.getElementById("render-content");
-  const canvas = await html2canvas(renderContent);
+  const imageContent = document.getElementById("image-content");
+  const canvas = await html2canvas(renderContent, {
+    width: imageContent.offsetWidth,
+    height: imageContent.offsetHeight,
+    scale: 1,
+  });
   const renderedImage = document.getElementById("rendered-image");
   renderedImage.src = canvas.toDataURL("image/jpeg");
 }
@@ -28,15 +33,14 @@ async function upload() {
   uploadButton.disabled = false;
 }
 
-const fileInput = document.getElementById("file");
-fileInput.addEventListener("change", (e) => {
+document.getElementById("file").addEventListener("change", (e) => {
   const file = e.target.files[0];
   if (!file) {
     return;
   }
   const reader = new FileReader();
   reader.onloadend = () => {
-    document.getElementById("inner-image").src = reader.result;
+    document.getElementById("inner-image").style.backgroundImage = `url("${reader.result}")`;
   };
   reader.readAsDataURL(file);
 });
