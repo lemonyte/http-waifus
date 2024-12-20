035bbfc55839b473f52b9dc41ea2fd6a8b15ee3b
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-12-20 01:42:22 -0800

    Fix typing issue

--- src/routes/[code=file]/+server.ts
+++ src/routes/[code=file]/+server.ts
@@ -1,6 +1,9 @@
 import type { RequestHandler } from "./$types";
 
 export const GET = (async ({ platform, url }) => {
+    if (platform === undefined) {
+        return new Response("Internal Server Error", { status: 500 });
+    }
     const obj = await platform.env.MEDIA.get(url.pathname.replaceAll("/", ""));
     if (obj === null) {
         return new Response("Not Found", { status: 404 });
