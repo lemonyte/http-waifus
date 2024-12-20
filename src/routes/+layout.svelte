d8a9cb8b86185f1fc4aca79d4d81d92f0de42c2a
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-12-20 02:13:03 -0800

    Add help wanted banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,12 @@
 </script>
 
 <div class="flex flex-col min-h-screen">
+    <div class="text-center p-4 bg-neutral-200 dark:bg-neutral-950">
+        <p>
+            <strong>Help wanted!</strong> This website needs <i><strong>your</strong></i> help to be completed! If you
+            have an image to add, please open an issue on <a href="https://github.com/lemonyte/http-waifus">GitHub</a> &#10084;
+        </p>
+    </div>
     <div class="flex flex-col items-center p-8 flex-grow">
         <a href="/" class="transition-all text-black dark:text-white hover:text-pink-400 dark:hover:text-pink-300">
             <h1 class="text-5xl font-bold mb-5">HTTP Waifus</h1>
