a8db28ddaa7961797e865622384b02c226483d8b
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2024-12-20 23:57:31 -0800

    Small layout fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,10 @@
     import { STATUS_CODES } from "$lib/codes";
 </script>
 
-<p class="text-neutral-600 dark:text-neutral-400 text-lg font-bold mb-4">
+<p class="text-center text-neutral-600 dark:text-neutral-400 text-lg font-bold mb-4">
     Waifus for every HyperText Transfer Protocol response status code
 </p>
-<div class="w-full my-4 mx-8 flex flex-col">
+<div class="w-full my-4 mx-8 px-6 flex flex-col">
     <span class="text-lg">API usage:</span>
     <code id="api-url" class="p-4 my-2">{page.url.origin}/&lbrace;code&rbrace;.jpeg</code>
     <span>Replace <code>&lbrace;code&rbrace;</code> with a status code.</span>
@@ -18,9 +18,9 @@
             <img
                 src={`/${code}.jpeg`}
                 alt="{code} {description}"
-                width="800"
-                height="700"
-                class="w-full h-64"
+                width={800}
+                height={700}
+                class="w-full h-64 rounded-xl"
                 loading="lazy"
             />
         </a>
