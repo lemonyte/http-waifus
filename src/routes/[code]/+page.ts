f07411acbae746b416fce07fe9052966184bec10
Lemonyte <49930425+lemonyte@users.noreply.github.com>
2025-04-29 18:18:46 -0700

    Remove redundant types

--- src/routes/[code]/+page.ts
+++ src/routes/[code]/+page.ts
@@ -1,8 +1,7 @@
 import { redirect } from "@sveltejs/kit";
-import type { PageLoad } from "./$types";
 import { STATUS_CODES } from "$lib/codes";
 
-export const load: PageLoad = ({ params }) => {
+export const load = ({ params }) => {
     const code = parseInt(params.code);
     if (code in STATUS_CODES) {
         return { code };
